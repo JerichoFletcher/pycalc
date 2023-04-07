3ee86495f69aec76ca060d735b05f1f1788b5c36
JerichoFletcher <jericho.russel17@gmail.com>
2023-04-08 02:22:56 +0700

    chore: setup py

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pycalc',
+    version='0.1',
+    packages=['cio', 'ops', 'modules'],
+    package_dir={'': 'src'},
+    url='https://github.com/JerichoFletcher/pycalc',
+    license='',
+    author='JerichoFletcher',
+    author_email='jericho.russel17@gmail.com',
+    description='A simple calculator program written in Python.'
+)
