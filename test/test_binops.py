a8cb499e106b51a7660e2f7c97a1c47ab35cad01
JerichoFletcher <jericho.russel17@gmail.com>
2023-04-07 22:35:01 +0700

    add: sub, mul, div

--- test/test_binops.py
+++ test/test_binops.py
@@ -1,7 +1,7 @@
 """Test script for testing binary operators."""
 
 import pytest
-from ops.binary import AddOperator
+from ops.binary import AddOperator, SubtractOperator, MultiplyOperator, DivideOperator
 
 
 def test_add() -> None:
@@ -17,3 +17,52 @@ def test_add() -> None:
         for second_op in range(-100, 100, 1):
             result = operator.eval(float(first_op), float(second_op))
             assert result == first_op + second_op
+
+
+def test_sub() -> None:
+    """Tests the behavior of the subtract operator."""
+    operator = SubtractOperator()
+    with pytest.raises(TypeError):
+        operator.eval('4', '6')
+    with pytest.raises(ValueError):
+        operator.eval(1.)
+    with pytest.raises(ValueError):
+        operator.eval(2., 4., 5.)
+    for first_op in range(-100, 100, 1):
+        for second_op in range(-100, 100, 1):
+            result = operator.eval(float(first_op), float(second_op))
+            assert result == first_op - second_op
+
+
+def test_mul() -> None:
+    """Tests the behavior of the multiply operator."""
+    operator = MultiplyOperator()
+    with pytest.raises(TypeError):
+        operator.eval('4', '6')
+    with pytest.raises(ValueError):
+        operator.eval(1.)
+    with pytest.raises(ValueError):
+        operator.eval(2., 4., 5.)
+    for first_op in range(-100, 100, 1):
+        for second_op in range(-100, 100, 1):
+            result = operator.eval(float(first_op), float(second_op))
+            assert result == first_op * second_op
+
+
+def test_div() -> None:
+    """Tests the behavior of the divide operator."""
+    operator = DivideOperator()
+    with pytest.raises(TypeError):
+        operator.eval('4', '6')
+    with pytest.raises(ValueError):
+        operator.eval(1.)
+    with pytest.raises(ValueError):
+        operator.eval(2., 4., 5.)
+    for first_op in range(-100, 100, 1):
+        for second_op in range(-100, 100, 1):
+            if second_op == 0.:
+                with pytest.raises(ZeroDivisionError):
+                    operator.eval(float(first_op), float(second_op))
+            else:
+                result = operator.eval(float(first_op), float(second_op))
+                assert result == first_op / second_op
